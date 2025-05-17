10c0abd39be7f9c69d1ea9049b30ef57cd08778c
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-05-17 09:56:44 -0300

    37

--- views/components/sidebar-store.php
+++ views/components/sidebar-store.php
@@ -103,7 +103,7 @@ if (!isset($activeMenu)) {
             </a>
         </li>
         <li>
-            <a href="<?php echo SITE_URL; ?>/stores/perfil" class="sidebar-nav-item <?php echo ($activeMenu == 'profile') ? 'active' : ''; ?>">
+            <a href="<?php echo SITE_URL; ?>/store/perfil" class="sidebar-nav-item <?php echo ($activeMenu == 'profile') ? 'active' : ''; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
