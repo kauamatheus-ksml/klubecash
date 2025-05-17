d51749a88d3671506da859fa6078cd4091cf2929
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-05-17 13:07:49 -0300

    43

--- controllers/TransactionController.php
+++ controllers/TransactionController.php
@@ -1059,8 +1059,10 @@ class TransactionController {
             ];
 
             // Aplicar filtro de status corretamente
-            $query .= " AND t.status = :status";
-            $params[':status'] = TRANSACTION_PENDING; // Usar a constante em vez da string literal
+            // Se quiser incluir transações tanto pendentes quanto em pagamento pendente
+            $query .= " AND (t.status = :status_pendente OR t.status = :status_pagamento_pendente)";
+            $params[':status_pendente'] = TRANSACTION_PENDING;
+            $params[':status_pagamento_pendente'] = TRANSACTION_PAYMENT_PENDING;// Usar a constante em vez da string literal
             
             // Aplicar filtros
             if (!empty($filters)) {
