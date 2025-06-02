b51019c7b84a90c39244514002d55988fc7414b2
Kaua <kauamatheus920@gmail.com>
2025-06-02 17:33:45 -0300

    351

--- views/admin/users.php
+++ views/admin/users.php
@@ -68,7 +68,6 @@ try {
     <link rel="shortcut icon" type="image/jpg" href="../../assets/images/icons/KlubeCashLOGO.ico"/>
     
     <!-- Font Awesome primeiro -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../assets/font-awesome/css/font-awesome.min.css">
 
     <!-- Seu CSS depois -->
