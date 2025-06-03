89c75a706f36803a905c81c4303ec4d54bf62e6b
Kaua <kauamatheus920@gmail.com>
2025-06-02 21:16:32 -0300

    365

--- api/openpix.php
+++ api/openpix.php
@@ -1,14 +1,13 @@
 <?php
 // api/openpix.php
-// Adicione temporariamente no início do api/openpix.php
-echo "API Key: " . OPENPIX_API_KEY;
-exit;
 
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
+// Adicione temporariamente no início do api/openpix.php
+echo "API Key: " . OPENPIX_API_KEY;
+exit;
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
