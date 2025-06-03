16658b6bfbd644ecf61c8f600b3d4e7236ad06fd
Kaua <kauamatheus920@gmail.com>
2025-06-03 00:46:49 -0300

    405

--- utils/MercadoPagoClient.php
+++ utils/MercadoPagoClient.php
@@ -74,8 +74,7 @@ class MercadoPagoClient {
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_SSL_VERIFYPEER => false, // Para teste apenas
-            CURLOPT_SSL_VERIFYHOST => false, // Para teste apenas
+            
             CURLOPT_USERAGENT => 'KlubeCash/1.0',
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
         ]);
