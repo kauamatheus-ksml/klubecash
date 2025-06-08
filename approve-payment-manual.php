b74cfebf6cda8c7b8b0dd7d0d72e9159b2846531
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-06-08 20:25:35 -0300

    u297617088_klubecash

--- approve-payment-manual.php
+++ approve-payment-manual.php
@@ -1,7 +1,7 @@
 <?php
 // approve-payment-manual.php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=u297617088_klubecash", "u297617088_root", "Aaku_2004@");
+    $db = new PDO("mysql:host=localhost;dbname=u297617088_klubecash", "u297617088_klubecash", "Aaku_2004@");
     
     // Verificar estrutura da tabela
     $stmt = $db->query("DESCRIBE cashback_saldos");
