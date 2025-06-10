3c7fac8ff7fe3fc1d9e6683b298b9d842c357662
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-06-10 12:44:30 -0300

    profile.css

--- views/client/profile.php
+++ views/client/profile.php
@@ -212,7 +212,7 @@ $cpfPendente = empty($profileData['perfil']['cpf']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Perfil - Klube Cash</title>
     <link rel="shortcut icon" type="image/jpg" href="../../assets/images/icons/KlubeCashLOGO.ico"/>
-    <link rel="stylesheet" href="../../assets/css/views/client/profile.css">
+    <link rel="stylesheet" href="../assets/css/views/client/profile.css">
     <!-- Font Awesome para ícones -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
