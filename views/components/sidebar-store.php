bdd488bb9d33d9886cc4828e0d58aafe642e3e78
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-06-16 08:31:42 -0300

    ocultartrasncoes

--- views/components/sidebar-store.php
+++ views/components/sidebar-store.php
@@ -68,6 +68,7 @@ if (!isset($activeMenu)) {
                 Registrar Venda
             </a>
         </li>
+        <!--
         <li>
             <a href="<?php echo STORE_TRANSACTIONS_URL; ?>" class="sidebar-nav-item <?php echo ($activeMenu == 'transactions') ? 'active' : ''; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -77,7 +78,7 @@ if (!isset($activeMenu)) {
                 </svg>
                 Transações
             </a>
-        </li>
+        </li>-->
         <!--<li>
             <a href="<?php echo STORE_BATCH_UPLOAD_URL; ?>" class="sidebar-nav-item <?php echo ($activeMenu == 'batch-upload') ? 'active' : ''; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
