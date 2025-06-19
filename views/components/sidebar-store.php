b2ea5d70084af4ab7f3b51d2e7bd3294468cab37
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-06-19 15:33:01 -0300

    <span>Comissões Pendentes</span>

--- views/components/sidebar-store.php
+++ views/components/sidebar-store.php
@@ -56,7 +56,7 @@ if (!isset($activeMenu)) {
                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                     <polyline points="9 22 9 12 15 12 15 22"></polyline>
                 </svg>
-                Inicio 
+                <span>Inicio</span> 
             </a>
         </li>
         <li>
@@ -65,7 +65,7 @@ if (!isset($activeMenu)) {
                     <line x1="12" y1="5" x2="12" y2="19"></line>
                     <line x1="5" y1="12" x2="19" y2="12"></line>
                 </svg>
-                Registrar Venda
+                <span>Registrar Venda</span>
             </a>
         </li>
         <li>
