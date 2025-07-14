13a3e7692fd606438293aee577894f90e04efd67
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-07-14 16:25:19 -0300

    [         'id' => 'funcionarios',         'title' => 'Funcionários',         'url' => STORE_EMPLOYEES_URL,         'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>'     ],

--- views/components/sidebar-store.php
+++ views/components/sidebar-store.php
@@ -41,6 +41,12 @@ $menuItems = [
         'url' => STORE_REGISTER_TRANSACTION_URL,
         'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>'
     ],
+    [
+        'id' => 'funcionarios', 
+        'title' => 'Funcionários', 
+        'url' => STORE_EMPLOYEES_URL,
+        'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>'
+    ],
     /*
     // VENDAS - OCULTADO CONFORME SOLICITADO
     [
