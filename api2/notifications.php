8136bafd608a496bf930a3da17e6f23945daae9c
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-07-22 17:26:23 -0300

    https://klubecash.com/api2/notifications.php?action=list&user_id=9

--- api2/notifications.php
+++ api2/notifications.php
@@ -1,10 +1,18 @@
 <?php
 require_once '../config/database.php';
 
+
+
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT');
+header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS' );
 header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
 $action = $_GET['action'] ?? '';
