97a2c3e24e13d1cd69c95754a1fdcf4bd59ed03f
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-07-22 17:32:53 -0300

    }

--- api2/notifications.php
+++ api2/notifications.php
@@ -1,15 +1,13 @@
 <?php
 require_once '../config/database.php';
 
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Max-Age: 86400');
 
-// ADICIONAR ESTA PARTE:
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
+    exit(0);
 }
 
 $method = $_SERVER['REQUEST_METHOD'];
