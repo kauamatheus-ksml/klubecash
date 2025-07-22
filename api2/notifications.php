09b4adc65e23eea0e0ff24e3b1f789ac0c1c1247
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-07-22 17:31:02 -0300

    <?php require_once '../config/database.php';
    header('Content-Type: application/json');
    header('Access-Control-Allow-Origin: *');
    header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
    header('Access-Control-Allow-Headers: Content-Type, Authorization');
    
    // ADICIONAR ESTA PARTE:
    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
        http_response_code(200);
        exit();
    }

--- api2/notifications.php
+++ api2/notifications.php
@@ -1,14 +1,12 @@
 <?php
 require_once '../config/database.php';
 
-
-
-
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS' );
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+// ADICIONAR ESTA PARTE:
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
