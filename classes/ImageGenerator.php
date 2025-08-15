efd200f00bf9b187ed7b6d9b3ba935a5f4989e8c
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-08-15 09:38:23 -0300

    410

--- classes/ImageGenerator.php
+++ classes/ImageGenerator.php
@@ -15,7 +15,7 @@ class ImageGenerator {
             
             // Dimensões da imagem
             $width = 600;
-            $height = 400;
+            $height = 410;
             
             // Criar imagem
             $image = imagecreatetruecolor($width, $height);
