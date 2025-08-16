e42a8275a7c6ed8295c1faad911b379738e7599c
kauamatheus-ksml <kauamatheus920@gmail.com>
2025-08-16 13:31:23 -0300

    claudecode

--- view-log.php
+++
@@ -1,8 +0,0 @@
-<?php
-// view-log.php (criar na raiz)
-if (file_exists('/tmp/openpix.log')) {
-    echo "<pre>" . file_get_contents('/tmp/openpix.log') . "</pre>";
-} else {
-    echo "Log não encontrado";
-}
-?>
