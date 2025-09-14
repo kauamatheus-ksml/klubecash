798cc0903eaafb31564f596cecf3e6f9866f5c0b
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-09-14 00:45:37 -0300

    dsds

--- config/constants.php
+++ config/constants.php
@@ -9,7 +9,6 @@ if (!defined('ROOT_PATH')) {
 
 
 
-
 // === TIPOS DE CLIENTE ===
 define('CLIENT_TYPE_COMPLETE', 'completo');      // Cliente com cadastro completo
 define('CLIENT_TYPE_VISITOR', 'visitante');      // Cliente visitante (sem senha)
