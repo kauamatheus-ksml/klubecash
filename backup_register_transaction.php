0b1c21fbd719f0c5ef42ca6fd0a1d07f6a04cf8d
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-09-24 02:59:10 -0300

    TESTE LIMPEZA

--- backup_register_transaction.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Backup da função registerTransaction original antes da correção
-// Data: 2025-08-28
-// Esta versão tinha problemas de integração que causavam "There is no active transaction"
-?>
