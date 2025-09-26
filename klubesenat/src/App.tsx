a1cc2b1818249d122da1569853ebb9a96134eaf5
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-09-26 17:03:15 -0300

    Use o sistema principal do Klube Cash para fazer login

--- klubesenat/src/App.tsx
+++ klubesenat/src/App.tsx
@@ -44,7 +44,7 @@ const App = () => (
               path="/historico"
               element={
                 <ProtectedRoute>
-                  <History />
+                  <Historico />
                 </ProtectedRoute>
               }
             />
