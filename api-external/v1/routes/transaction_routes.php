91a871f07102cb980cbd6a049b1447589db8c7e2
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-09-26 15:02:34 -0300

    limpando

--- api-external/v1/routes/transaction_routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Rotas de transações
-$router->get('/', 'TransactionController@listTransactions');
-$router->get('/stats', 'TransactionController@getTransactionStats');
-$router->get('/{id}', 'TransactionController@getTransaction');
-$router->post('/', 'TransactionController@createTransaction');
-$router->put('/{id}/status', 'TransactionController@updateTransactionStatus');
-?>
